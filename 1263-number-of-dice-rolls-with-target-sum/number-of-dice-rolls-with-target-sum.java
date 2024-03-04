ec2e9cab55a51f1e4e69dbfa16ebc4464abcac5f
Kanishk Sharma <kanishksharma809@gmail.com>
2024-03-04 23:28:40 +0530

    Time: 9 ms (87.69%) | Memory: 41.5 MB (82.58%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -1,26 +1,25 @@
 class Solution {
 
-    private int MOD = 1000000007;
+    private static final int MOD = 1000000007;
 
     public int numRollsToTarget(int n, int k, int target) {
         int[][] dp = new int[n+1][target+1];
         for(int[] row: dp)
             Arrays.fill(row, -1);
-        numRollsToTarget(n, k, target, dp);
-        return dp[n][target];
+        return numRollsToTarget(n, k, target, dp);
     }
 
-    private int numRollsToTarget(int dice, int faces, int target, int[][] dp) {
-        if(target<0 || (target==0 && dice!=0))
+    private int numRollsToTarget(int n, int k, int target, int[][] dp) {
+        if(target==0 && n==0)
+            return 1;
+        if(target<=0 || n<=0)
             return 0;
-        if(dice==0)
-            return target==0 ? 1 : 0;
-        if(dp[dice][target]!=-1)
-            return dp[dice][target];
-        int ans = 0;
-        for(int i=1; i<=faces; i++) {
-            ans = (ans + numRollsToTarget(dice-1, faces, target-i, dp)%MOD)%MOD;
+        if(dp[n][target]!=-1)
+            return dp[n][target];
+        long ans = 0;
+        for(int i=1; i<=k; i++) {
+            ans += (numRollsToTarget(n-1, k, target-i, dp)%MOD);
         }
-        return dp[dice][target] = ans;
+        return dp[n][target] = (int)(ans%MOD);
     }
 }
